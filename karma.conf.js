aac3432904297f7a8edffebd6cd3f6425274bedb
Alexandr Balasyan <mywebstreet@gmail.com>
2018-03-22 13:58:46 +0300

    version 0.6.0

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
       'test/main.js': ['webpack', 'sourcemap']
     },
     webpack: {
+      watch: false,
       devtool: 'inline-source-map',
       module: {
         rules: [
