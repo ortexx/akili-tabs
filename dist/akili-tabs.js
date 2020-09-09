3f898745ca6291c1ad6ec60e5d211cbeafdd88b3
Alexandr Balasyan <mywebstreet@gmail.com>
2020-09-09 12:05:40 +0300

    upgraded deps

--- dist/akili-tabs.js
+++ dist/akili-tabs.js
@@ -1,7 +1,7 @@
 /*!
  * Tabs component for Akili framework
  * 
- * @version 1.1.3
+ * @version 1.1.4
  * @author Alexandr Balasyan <mywebstreet@gmail.com>
  * {@link https://github.com/ortexx/akili-tabs}
  * {@link https://github.com/ortexx/akili}
